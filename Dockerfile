ebb8255f0d9854b84e2a44f6ae80e62adebc40f0
CI-test-1 <CI-test-1@qq.com>
2016-04-18 09:15:35 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM daocloud/ci-golang:1.4
+
+MAINTAINER luffyma <luffyma@tencent.com>
---
+++ aTest_test.go
@@ -0,0 +1,15 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestFuncA(t *testing.T) {
+	t.Log("Test A ...")
+	t.Log(" ... Passed")
+}
+
+func TestFuncB(t *testing.T) {
+	t.Log("Test B ...")
+	t.Log(" ... Passed")
+}
